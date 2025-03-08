0812f918a63d6832aedbb75cbf064b9bd56e8955
ali_a <alialineejad@gmail.com>
2025-03-08 14:30:03 +0330

    create a phone for Ui

--- app/home/DataSection/page.js
+++ app/home/DataSection/page.js
@@ -1,5 +1,5 @@
-"use client";
-
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
 import {
     Table,
     TableHeader,
@@ -7,293 +7,166 @@ import {
     TableBody,
     TableRow,
     TableCell,
-    User,
-    Chip,
-    Button,
     Avatar,
+    Chip,
+    Image,
 } from "@heroui/react";
 import { useTheme } from "next-themes";
-import React, { useState, useCallback } from "react"; 
-import Chart from "react-apexcharts";
-export const columns = [
-    { name: "Name", uid: "name" },
-    { name: "Last Price", uid: "lastPrice" },
-    { name: "24h Change", uid: "Change24" },
-    { name: "Chart", uid: "chart" },
-    { name: "24h Volume", uid: "volume24h" },
-    { name: "Action", uid: "Action" },
-];
 
 export default function DataSection() {
-    const { theme } = useTheme(); 
+    const { theme } = useTheme();
+    const isDark = theme === "dark";
 
-    const isDark = theme === 'dark';
-     const data = [
+    const [ref, inView] = useInView({
+        threshold: 0.5,
+        triggerOnce: true
+    });
+
+    const data = [
         {
             id: 1,
             name: "BTC",
-            lastPrice: '88.934',
-            Change24: '-0.05',
-            icon: 'coins/icons8-bitcoin-240.png',
-            Chart: {
-                last12h: {
-                    ago1h: 82120,
-                    ago2h: 83120,
-                    ago3h: 85120,
-                    ago4h: 85820,
-                    ago5h: 84320,
-                    ago6h: 86120,
-                    ago7h: 86220,
-                    ago8h: 88120,
-                    ago9h: 89120,
-                    ago10h: 90120,
-                    ago11h: 91120,
-                    ago12h: 90120,
-                }
-            },
-            marketCap: 1.7,
-            volume24h: 79.9
+            lastPrice: "88.934",
+            Change24: "-0.05",
+            icon: "coins/icons8-bitcoin-240.png",
+           
         },
         {
             id: 2,
             name: "ETH",
-            icon: 'coins/icons8-ethereum-240.png',
-    
-            lastPrice: '2.670',
-            Change24: '+1.25',
-            Chart: {
-                last12h: {
-                    ago1h: 2650,
-                    ago2h: 2660,
-                    ago3h: 2665,
-                    ago4h: 2668,
-                    ago5h: 2672,
-                    ago6h: 2675,
-                    ago7h: 2674,
-                    ago8h: 2670,
-                    ago9h: 2665,
-                    ago10h: 2662,
-                    ago11h: 2660,
-                    ago12h: 2655,
-                }
-            },
-            marketCap: 0.3,
-            volume24h: 20.5
+            lastPrice: "2.670",
+            Change24: "+1.25",
+            icon: "coins/icons8-ethereum-240.png",
+
         },
         {
             id: 3,
-            name: "LTC",
-            lastPrice: '150',
-            icon: 'coins/icons8-litecoin-250.png',
-    
-            Change24: '+0.85',
-            Chart: {
-                last12h: {
-                    ago1h: 149,
-                    ago2h: 138,
-                    ago3h: 151,
-                    ago4h: 152,
-                    ago5h: 150,
-                    ago6h: 149,
-                    ago7h: 150,
-                    ago8h: 151,
-                    ago9h: 150,
-                    ago10h: 149,
-                    ago11h: 150,
-                    ago12h: 148,
-                }
-            },
-            marketCap: 0.01,
-            volume24h: 5.2
+            name: "BTC",
+            lastPrice: "88.934",
+            Change24: "-0.05",
+            icon: "coins/icons8-bitcoin-240.png",
+           
         },
         {
             id: 4,
-            name: "XRP",
-            icon: 'coins/icons8-xrp-240.png',
-    
-            lastPrice: '0.75',
-            Change24: '+0.20',
-            Chart: {
-                last12h: {
-                    ago1h: 0.74,
-                    ago2h: 0.73,
-                    ago3h: 0.75,
-                    ago4h: 0.76,
-                    ago5h: 0.77,
-                    ago6h: 0.76,
-                    ago7h: 0.68,
-                    ago8h: 0.754,
-                    ago9h: 0.73,
-                    ago10h: 0.72,
-                    ago11h: 0.74,
-                    ago12h: 0.73,
-                }
-            },
-            marketCap: 0.04,
-            volume24h: 1.5
+            name: "ETH",
+            lastPrice: "2.670",
+            Change24: "+1.25",
+            icon: "coins/icons8-ethereum-240.png",
+
         },
         {
             id: 5,
-            name: "ADA",
-            icon: 'coins/icons8-exchange-96.png',
-    
-            lastPrice: '1.10',
-            Change24: '-0.15',
-            Chart: {
-                last12h: {
-                    ago1h: 1.09,
-                    ago2h: 1.08,
-                    ago3h: 1.11,
-                    ago4h: 1.10,
-                    ago5h: 1.12,
-                    ago6h: 1.11,
-                    ago7h: 1.10,
-                    ago8h: 1.09,
-                    ago9h: 1.08,
-                    ago10h: 1.07,
-                    ago11h: 1.09,
-                    ago12h: 1.08,
-                }
-            },
-            marketCap: 0.03,
-            volume24h: 3.2
+            name: "BTC",
+            lastPrice: "88.934",
+            Change24: "-0.05",
+            icon: "coins/icons8-bitcoin-240.png",
+           
         },
+        {
+            id: 6,
+            name: "ETH",
+            lastPrice: "2.670",
+            Change24: "+1.25",
+            icon: "coins/icons8-ethereum-240.png",
+
+        },
+        {
+            id: 7,
+            name: "BTC",
+            lastPrice: "88.934",
+            Change24: "-0.05",
+            icon: "coins/icons8-bitcoin-240.png",
+           
+        },
+        {
+            id: 8,
+            name: "ETH",
+            lastPrice: "2.670",
+            Change24: "+1.25",
+            icon: "coins/icons8-ethereum-240.png",
+
+        },
+        {
+            id: 9,
+            name: "BTC",
+            lastPrice: "88.934",
+            Change24: "-0.05",
+            icon: "coins/icons8-bitcoin-240.png",
+           
+        }
     ];
-    const getChartData = (chartData) => {
-        const labels = Object.keys(chartData.last12h).map(key => key.replace('ago', ''));
-        const values = Object.values(chartData.last12h);
-
-        return {
-            series: [{
-                name: 'Price',
-                data: values,
-            }],
-            options: {
-                chart: {
-                    type: 'area',
-                    height: 60,
-                    width: 100,
-                    sparkline: {
-                        enabled: true,
-                    },
-                },
-                stroke: {
-                    curve: 'smooth',
-                    width: 2,
-                    colors: ['#018c81'],
-                },
-                fill: {
-                    type: 'gradient',
-                    gradient: {
-                        shade: '',
-                        type: 'vertical',
-                        shadeIntensity: 10,
-                        inverseColors: false,
-                        stops: [0, 100],
-                    },
-                },
-                yaxis: {
-                    labels: {
-                        style: {
-                            colors: '#018c81',
-                        },
-                        offsetX: 0,
-                    },
-                    axisBorder: {
-                        show: false,
-                    },
-                    axisTicks: {
-                        show: false,
-                    },
-                },
-                xaxis: {
-                    categories: labels,
-                    labels: {
-                        show: true,
-                    },
-                },
-                tooltip: {
-                    enabled: true,
-                    shared: true,
-                },
-                legend: {
-                    show: false,
-                },
-                grid: {
-                    borderColor: '#018c81',
-                    row: {
-                        colors: ['transparent'],
-                    },
-                },
-            },
-        };
-    };
-
-    const renderCell = useCallback((item, columnKey) => { // useCallback here
+    // رندر کردن سلول‌ها (همان تابع قبلی)
+    const renderCell = (item, columnKey) => {
         const cellValue = item[columnKey];
 
         switch (columnKey) {
             case "name":
-                return (
-                    <Avatar src={item.icon} className="font-bold text-xl bg-transparent" name={cellValue}  > {item.name}</Avatar>
-                );
+                return <Avatar src={item.icon} className="font-bold text-md bg-transparent" name={cellValue} />;
             case "lastPrice":
-                return <p className="font-bold text-lg">{cellValue} $</p>;
+                return <p className="font-bold text-md">{cellValue} $</p>;
             case "Change24":
                 return (
-                    <Chip className=" font-bold text-large" color={cellValue > 0 ? 'success' : 'danger'} variant="light">
+                    <Chip className="font-bold" color={cellValue > 0 ? "success" : "danger"} variant="light">
                         {cellValue}%
                     </Chip>
-
                 );
-            case "volume24h":
-                return (
-                    <p className=" font-bold text-large"  >
-                        {cellValue} B
-                    </p>
-
-                );
-            case "chart":
-                const chartData = getChartData(item.Chart);
-                return (
-                    <div style={{ width: '100px', height: '60px' }}>
-                        <Chart options={chartData.options} series={chartData.series} type="line" height={60} />
-                    </div>
-                );
-            case "Action":
-                return (
-                    <p className=" font-bold text-large"  >
-                        <Button className="border-teal-700 text-gray-400 border-[2px] font-bold hover:scale-95" variant="faded">Show</Button>
-                    </p>
-
-                );
-
             default:
                 return cellValue;
         }
-    }, [getChartData]); // Dependencies for useCallback
+    };
+    const phoneVariants = {
+        hidden: { opacity: 0, y: 200 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }
+    };
+
+    const tableVariants = {
+        hidden: { opacity: 0, scale: 0.8 },
+        visible: { opacity: 1, scale: 1, transition: { delay: 0.3, duration: 0.5 } }
+    };
 
     return (
-        <div className={`${isDark ? 'bg-gradient-to-b from-black to-gray-900' : 'bg-gradient-to-b from-teal-50 to-white'} text-${isDark ? 'white' : 'gray-800'} h-[70vh] `}>
-        <Table
-            aria-label="Your Table Description Here"
-            className={` rounded-none  mt-10 mx-auto max-mdu:-mt-12 z-20 w-[65%]  shadow-none`}
+        <div
+            ref={ref}
+            className={`relative flex justify-center items-center min-h-screen ${
+                isDark ? "bg-gradient-to-b from-black to-gray-900" : "bg-gradient-to-b from-white to-teal-50"
+            }`}
         >
-            <TableHeader columns={columns}>
-                {(column) => (
-                    <TableColumn key={column.uid} align={column.uid === "Action" ? "center" : "start"}>
-                        {column.name}
-                    </TableColumn>
-                )}
-            </TableHeader>
-            <TableBody className="bg-gray-900" items={data} >
-                {(item) => (
-                    <TableRow key={item.id} className={` ${isDark ? 'border-b-1 border-teal-900' : ''}`}>
-                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
+            <motion.div 
+                className="z-0"
+                initial="hidden"
+                animate={inView ? "visible" : "hidden"}
+                variants={phoneVariants}
+            >
+                <Image src="./Svgs/phoneSVG.png" alt="Phone Frame" className="w-[440px]" />
+            </motion.div>
+
+            <motion.div 
+                className="absolute z-10 bg-transparent"
+                initial="hidden"
+                animate={inView ? "visible" : "hidden"}
+                variants={tableVariants}
+            >
+                <Table
+                    aria-label="Your Table Description Here"
+                    className="overflow-y-hidden bg-transparent shadow-inner rounded-b-[120px] text-gray-800 dark:text-white mx-auto w-[270px] h-[630px] z-0"
+                >
+                    <TableHeader>
+                        <TableColumn>Name</TableColumn>
+                        <TableColumn>Last Price</TableColumn>
+                        <TableColumn>24h Change</TableColumn>
+                    </TableHeader>
+                    <TableBody items={data}>
+                        {(item) => (
+                            <TableRow key={item.id} className="text-sm">
+                                {["name", "lastPrice", "Change24"].map((columnKey) => (
+                                    <TableCell className="text-sm" key={columnKey}>{renderCell(item, columnKey)}</TableCell>
+                                ))}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </motion.div>
         </div>
     );
 }
